9b181a8c0f06deb3eec1a6004d410d4d043fe912
keemsy <phzzz@naver.com>
2024-04-05 17:30:45 +0900

    PT-8 Add: 도메인 모델 추가 - Question, Answer 추가

---
+++ src/domains/answer/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from src.database import Base
+
+"""
+question = relationship("Question", backref="answers")
+
+- relationship 은 두 테이블 간의 관계를 나타낸다.
+  - 첫번째 파라미터: 참조할 모델명
+  - backref: 역참조 설정 ex. Question 객체 A에서 A.answers로 A에 달린 답변을 참조
+"""
+
+
+class Answer(Base):
+    __tablename__ = "answer"
+
+    id = Column(Integer, primary_key=True)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
+    question_id = Column(Integer, ForeignKey("question.id"))
+    question = relationship("Question", backref="answers")
