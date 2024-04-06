7e69a04e12578a9fee59693b10d992a946fd6498
keemsy <phzzz@naver.com>
2024-04-07 02:25:12 +0900

    PT-8 Add: Pydantic Schema 모델 추가 및 반영 - Pydantic을 활용한 Question Schema 추가 - question/router.py 내 Pydantic 적용(작성한 Question Schema 모델 적용() - pydantic V1 사용으로 인한 orm_mode 코드 작성(V2에서는 불필요)

--- src/domains/question/router.py
+++ src/domains/question/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from src.database import get_db
+from src.domains.question import schemas
 from src.domains.question.models import Question
 
 router = APIRouter(
@@ -9,7 +10,7 @@ router = APIRouter(
 )
 
 
-@router.get("/list")
+@router.get("/list", response_model=list[schemas.Question])
 def question_list(db: Session = Depends(get_db)):
     _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
     return _question_list
