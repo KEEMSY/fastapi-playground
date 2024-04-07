d6bef131c1238ec7b6ca69285ad38b3e2e43b478
keemsy <phzzz@naver.com>
2024-04-07 18:00:23 +0900

    PT-8 Add: 질문 등록(QuestionCreate)을 위한 frontend 코드 추가 - App.svelte 내 신규 route 추가 - Home.svelte 내 질문 추가를 위한 코드 추가 - 질문 등록을 위한 페이지(QuestionCreate.svelte) 추가

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,11 +2,13 @@
   import Router from 'svelte-spa-router'
   import Home from "./routes/Home.svelte"
   import Detail from "./routes/Detail.svelte"
+  import QuestionCreate from "./routes/QuestionCreate.svelte";
 
 
   const routes = {
-    '/': Home,
-    '/detail/:question_id': Detail,
+      '/': Home,
+      '/detail/:question_id': Detail,
+      '/question-create': QuestionCreate,
   }
 </script>
 
