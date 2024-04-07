577d68f3434f31d334c4aea54522767b5afd418b
keemsy <phzzz@naver.com>
2024-04-07 11:49:11 +0900

    PT-8 Edit: SQLAlchemy에서 DB Key의 명명규칙을 명시적으로 사용하도록 수정 - database.py 내 naming convention 추가

--- src/database.py
+++ src/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -22,8 +22,15 @@ SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:test@localhost:13306/fast
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
-
+MYSQL_INDEXES_NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+metadata = MetaData(naming_convention=MYSQL_INDEXES_NAMING_CONVENTION)
+Base = declarative_base(metadata=metadata)
 
 def get_db():
     db = SessionLocal()
