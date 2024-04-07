a09c8c206f27b9a02ebbbd46649ec948f7021223
keemsy <phzzz@naver.com>
2024-04-07 17:17:41 +0900

    PT-8 Add: Pydantic을 활용한 Answer 추가 - question_schema 내 answers 필드 추가

--- src/domains/answer/schemas.py
+++ src/domains/answer/schemas.py
@@ -1,3 +1,5 @@
+import datetime
+
 from pydantic import BaseModel, field_validator
 
 
@@ -9,3 +11,9 @@ class AnswerCreate(BaseModel):
         if not v or not v.strip():
             raise ValueError("Content can't be empty")
         return v
+
+
+class Answer(BaseModel):
+    id: int
+    content: str
+    create_date: datetime.datetime
--- src/domains/question/schemas.py
+++ src/domains/question/schemas.py
@@ -2,9 +2,12 @@ import datetime
 
 from pydantic import BaseModel
 
+from src.domains.answer.schemas import Answer
+
 
 class Question(BaseModel):
     id: int
     subject: str
     content: str
     create_date: datetime.datetime
+    answers: list[Answer] = []
