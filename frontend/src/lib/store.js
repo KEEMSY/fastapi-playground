7eac23c5b8b19a465254f14e83b1899c3f331872
keemsy <phzzz@naver.com>
2024-04-09 22:13:23 +0900

    PT-8 Edit: store.js 내 중복 선언된 변수 제거

--- frontend/src/lib/store.js
+++ frontend/src/lib/store.js
@@ -1,7 +1,5 @@
 import { writable } from 'svelte/store'
 
-export const page = writable(0)
-
 const persist_storage = (key, initValue) => {
     const storedValueStr = localStorage.getItem(key)
     const store = writable(storedValueStr != null ? JSON.parse(storedValueStr) : initValue)
