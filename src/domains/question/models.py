6e766800f0410e03fccfbc22e5f50bd31166d9d4
keemsy <phzzz@naver.com>
2024-04-13 16:02:51 +0900

    PT-8 Add: question_voter 테이블(질문 추천) 생성 - Question Model 내 voter 속성 추가 - 마이그레이션 파일 추가

--- src/domains/question/models.py
+++ src/domains/question/models.py
@@ -1,7 +1,15 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 from src.database import Base
+
+
+question_voter = Table(
+    'question_voter',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
+    Column('question_id', Integer, ForeignKey('question.id'), primary_key=True)
+)
 
 
 class Question(Base):
@@ -14,3 +22,4 @@ class Question(Base):
     modify_date = Column(DateTime, nullable=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
     user = relationship("User", backref="question_users")
+    voter = relationship('User', secondary=question_voter, backref='question_voters')
