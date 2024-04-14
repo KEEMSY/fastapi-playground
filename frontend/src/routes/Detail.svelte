ba70037b8fa2b2a32f4bf58b46be66538112594a
keemsy <phzzz@naver.com>
2024-04-14 15:56:19 +0900

    PT-8 Add: frontend 내 답변 추천 버튼 및 추천 기능 추가 - 개선 요소: 중복 추천에 대한 처리가 없음

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -90,6 +90,23 @@
         }
     }
 
+    function vote_answer(answer_id) {
+        if(window.confirm('정말로 추천하시겠습니까?')) {
+            let url = "/api/answer/vote"
+            let params = {
+                answer_id: answer_id
+            }
+            fastapi('post', url, params,
+                (json) => {
+                    get_question()
+                },
+                (err_json) => {
+                    error = err_json
+                }
+            )
+        }
+    }
+
 </script>
 
 <div class="container my-3">
@@ -149,6 +166,11 @@
                 {/if}
             </div>
             <div class="my-3">
+                <button class="btn btn-sm btn-outline-secondary"
+                    on:click="{vote_answer(answer.id)}">
+                    추천
+                    <span class="badge rounded-pill bg-success">{ answer.voter.length }</span>
+                </button>
                 {#if answer.user && $username === answer.user.username }
                 <a use:link href="/answer-modify/{answer.id}"
                     class="btn btn-sm btn-outline-secondary">수정</a>
