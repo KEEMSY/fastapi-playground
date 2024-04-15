44d8e4b6f1d332b68d274b05135a06d8696fbad4
keemsy <phzzz@naver.com>
2024-04-15 20:13:41 +0900

    PT-8 Edit: 질문 검색 시, 키워드를 포함하는 질문과 Answer을 찾도록 개선

--- src/domains/question/service.py
+++ src/domains/question/service.py
@@ -1,18 +1,33 @@
 from datetime import datetime
 
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
+from src.domains.answer.models import Answer
 from src.domains.question.models import Question
 from src.domains.question.schemas import QuestionCreate, QuestionUpdate
 from src.domains.user.models import User
 
 
 # offset: 시작 위치, limit: 가져올 데이터 수
-def get_question_list(db: Session, offset: int = 0, limit: int = 10):
-    _question_list = db.query(Question).order_by(Question.create_date.desc())
-
-    total = _question_list.count()
-    question_list = _question_list.offset(offset).limit(limit).all()
+def get_question_list(db: Session, offset: int = 0, limit: int = 10, keyword: str = ''):
+    question_list = db.query(Question)
+    if keyword:
+        search = '%%{}%%'.format(keyword)
+        sub_query = db.query(Answer.question_id, Answer.content, User.username) \
+            .outerjoin(User, and_(Answer.user_id == User.id)).subquery()
+        question_list = question_list \
+            .outerjoin(User) \
+            .outerjoin(sub_query, and_(sub_query.c.question_id == Question.id)) \
+            .filter(Question.subject.ilike(search) |        # 질문제목
+                    Question.content.ilike(search) |        # 질문내용
+                    User.username.ilike(search) |           # 질문작성자
+                    sub_query.c.content.ilike(search) |     # 답변내용
+                    sub_query.c.username.ilike(search)      # 답변작성자
+                    )
+    total = question_list.distinct().count()
+    question_list = question_list.order_by(Question.create_date.desc())\
+        .offset(offset).limit(limit).distinct().all()
     return total, question_list  # (전체 건수, 페이징 적용된 질문 목록)
 
 
