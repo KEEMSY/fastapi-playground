5ff644c16f692d7f02f75cf78826696c4e447305
keemsy <phzzz@naver.com>
2024-04-20 17:20:05 +0900

    PT-13 Add: 각 계층 별 에러를 Global로 처리하는 예외 핸들러 추가

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
 
 from src.domains.question import router as question_router
 from src.domains.answer import router as answer_router
 from src.domains.user import router as user_router
+from src.exceptions import PLException, BLException, DLException
 
 app = FastAPI()
 
@@ -22,6 +24,27 @@ app.add_middleware(
 app.include_router(question_router.router)
 app.include_router(answer_router.router)
 app.include_router(user_router.router)
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
+
+
+@app.exception_handler(PLException)
+async def pl_exception_handler(request, exc):
+    return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
+
+
+@app.exception_handler(BLException)
+async def bl_exception_handler(request, exc):
+    return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
+
+
+@app.exception_handler(DLException)
+async def dl_exception_handler(request, exc):
+    return JSONResponse(status_code=exc.status_code,
+                        content={"code": exc.code, "detail": exc.detail})
 
 
 @app.get("/")
