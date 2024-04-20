767625d6d8e6948fcd07944d7f6e1eaaca75132b
keemsy <phzzz@naver.com>
2024-04-20 17:18:50 +0900

    PT-13 Add: 각 계층 별 예외 추가 - PLException: Presentation Layer에서 발생한 예외 처리 - BLException: Business Layer애서 발생한 예외 처리 - DLException: Database Layer에서 발생한 예외 처리

---
+++ src/exceptions.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+class PLException(Exception):
+    def __init__(self, detail: Optional[str], status_code: int = 500, code: str = "D0000"):
+        self.code = code
+        self.detail = detail
+        self.status_code = status_code
+        super().__init__(detail)
+
+
+class BLException(Exception):
+    def __init__(self, detail: Optional[str], status_code: int = 500, code: str = "D0000"):
+        self.code = code
+        self.detail = detail
+        self.status_code = status_code
+        super().__init__(detail)
+
+
+class DLException(Exception):
+    def __init__(self, detail: Optional[str], status_code: int = 500, code: str = "D0000"):
+        self.code = code
+        self.detail = detail
+        self.status_code = status_code
+        super().__init__(detail)
