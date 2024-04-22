2476e408685c0de4f2c0d843caea9a87463f82b8
keemsy <phzzz@naver.com>
2024-04-22 20:11:25 +0900

    PT-14 Add: async_example/presentation.no_login_router.py 내 엔드포인트 추가 - 조회 엔드포인트 추가

--- src/domains/async_example/presentation/no_login_router.py
+++ src/domains/async_example/presentation/no_login_router.py
@@ -27,4 +27,13 @@ async def save_async_example(request: CreateAsyncExample, db: AsyncSession = Dep
         raise PLException(status_code=500, detail=str(e))
 
 
+@router.get("/example/{example_id}", response_model=AsyncExampleResponse, tags=["with_no_login_async_example"])
+async def get_async_example(example_id: int, db: AsyncSession = Depends(get_async_db)):
+    try:
+        async_example = await async_example_service.read_async_example(db, example_id)
+        return AsyncExampleResponse.model_validate(async_example)
+    except BLException as e:
+        raise PLException(status_code=400, detail=e.detail, code=e.code)
 
+    except Exception as e:
+        raise PLException(status_code=500, detail=str(e))
