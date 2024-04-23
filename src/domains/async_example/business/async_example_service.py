9e94cd868a1d3fba5bdb0d41108c5ab9f0bd8265
keemsy <phzzz@naver.com>
2024-04-23 20:33:40 +0900

    PT-14 Add: async_example_service.py 내 AsyncExample 업데이트 메서드 추가 - V1: 직접 id를 입력 받는 방법 - V2: Input Schema 내 필드 값으로만 이용하는 방법

--- src/domains/async_example/business/async_example_service.py
+++ src/domains/async_example/business/async_example_service.py
@@ -3,8 +3,10 @@ import logging
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.domains.async_example.business.schemas import AsyncExampleSchema
+from src.domains.async_example.constants import BLErrorCode
 from src.domains.async_example.database import async_example_crud
-from src.domains.async_example.presentation.schemas import CreateAsyncExample
+from src.domains.async_example.presentation.schemas import CreateAsyncExample, UpdateAsyncExampleV1, \
+    UpdateAsyncExampleV2
 from src.exceptions import DLException, BLException
 
 logger = logging.getLogger(__name__)
@@ -27,7 +29,7 @@ async def create_async_example_with_no_user(db: AsyncSession, example_create: Cr
 
 async def read_async_example(db: AsyncSession, async_example_id: int) -> AsyncExampleSchema:
     try:
-        async_example: AsyncExampleSchema = await async_example_crud\
+        async_example: AsyncExampleSchema = await async_example_crud \
             .read_fetch_async_example_with_user(db, async_example_id)
         return async_example
 
@@ -38,3 +40,37 @@ async def read_async_example(db: AsyncSession, async_example_id: int) -> AsyncEx
     except Exception as e:
         logger.error(f"Unexpected error while retrieving AsyncExample: {str(e)}")
         raise BLException(detail="An unexpected error occurred while retrieving AsyncExample")
+
+
+async def fetch_async_example_with_user_v1(db: AsyncSession, async_example_id: int,
+                                           request: UpdateAsyncExampleV1) -> AsyncExampleSchema:
+    try:
+        async_example: AsyncExampleSchema = await async_example_crud \
+            .update_async_example(db, async_example_id, request)
+        return async_example
+
+    except DLException as de:
+        logger.error(f"Failed to retrieve AsyncExample: {de.detail}")
+        raise BLException(code=de.code, detail=de.detail)
+
+    except Exception as e:
+        logger.error(f"Unexpected error while retrieving AsyncExample: {str(e)}")
+        raise BLException(code=BLErrorCode.UNKNOWN_ERROR,
+                          detail="An unexpected error occurred while retrieving AsyncExample")
+
+
+async def fetch_async_example_with_user_v2(db: AsyncSession, request: UpdateAsyncExampleV2) -> AsyncExampleSchema:
+    try:
+        example_id = request.async_example_id
+        async_example: AsyncExampleSchema = await async_example_crud \
+            .update_async_example(db, example_id, request)
+        return async_example
+
+    except DLException as de:
+        logger.error(f"Failed to retrieve AsyncExample: {de.detail}")
+        raise BLException(code=de.code, detail=de.detail)
+
+    except Exception as e:
+        logger.error(f"Unexpected error while retrieving AsyncExample: {str(e)}")
+        raise BLException(code=BLErrorCode.UNKNOWN_ERROR,
+                          detail=f"An unexpected error occurred while retrieving AsyncExample: {e}")
