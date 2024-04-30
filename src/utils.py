78bf4ab8f2d80a696dfb53d26c0bcd3f6bf30f39
keemsy <phzzz@naver.com>
2024-04-30 21:31:45 +0900

    PT-14 Edit: utils.py 수정(logging) - 스크립트가 실행되는 위치에 상관없이 utils.py 파일 위치를 기준으로 logging.ini에 대한 경로 확인을 수정

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,12 @@
+import os
 import logging.config
 
-logging.config.fileConfig('./logging.ini', disable_existing_loggers=False)
 
+def setup_logging():
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    logging_config_path = os.path.join(base_dir, 'logging.ini')
+    logging.config.fileConfig(logging_config_path, disable_existing_loggers=False)
+
+
+setup_logging()
 logger = logging.getLogger('uvicorn')
