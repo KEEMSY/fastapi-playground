677f933810f3eeb9c4733dc9264fdc17ade00e38
keemsy <phzzz@naver.com>
2024-05-02 21:14:20 +0900

    PT-19 Add: conftest.py 내 비동기 테스트 코드 관련 추가 - 비동기 테스트임을 명시하도록 코드 개선 - 테스트 코드 작성 간 에러 직면: attributeerror: 'async_generator' object has no attribute 'post'   - 해결: @pytest_asyncio.fixture 를 사용하여 해결 - 참고한 자료   - https://stackoverflow.com/questions/78079908/attributeerror-async-generator-object-has-no-attribute-post   -https://stackoverflow.com/questions/74351637/pytest-with-async-tests-test-setup-before-and-after

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 import pytest
+import pytest_asyncio
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from starlette.testclient import TestClient
 
-from src.database import Base, get_db
+from src.database import Base, get_db, get_async_db
 from src.main import app
 
 from sqlalchemy import create_engine
@@ -38,3 +41,35 @@ def sync_client(sync_session):
     app.dependency_overrides[get_db] = override_get_db
 
     yield TestClient(app)
+
+
+ASYNC_SQLALCHEMY_DATABASE_URL = "mysql+asyncmy://root:test@localhost:3306/fastapi_test"
+# async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL, echo=True)
+async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL)
+
+AsyncTestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
+)
+
+
+@pytest_asyncio.fixture
+async def async_session():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    async_session = AsyncTestingSessionLocal()
+    try:
+        yield async_session
+    finally:
+        await async_session.close()
+
+
+@pytest_asyncio.fixture
+async def async_client(async_session):
+    async def override_get_db():
+        yield async_session
+
+    app.dependency_overrides[get_async_db] = override_get_db
+
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
