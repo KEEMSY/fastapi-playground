ffb713af8eb470ae8a7ee1fa951c1216b12ea2ad
keemsy <phzzz@naver.com>
2024-05-02 21:07:10 +0900

    PT-19 Edit: conftest.py 내 동기 테스트 코드 관련 수정 - 동기 테스트임을 명시하도록 코드 개선

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,14 +7,15 @@ from src.main import app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test@localhost:3306/fastapi_test"
+SYNC_SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test@localhost:3306/fastapi_test"
 
-sync_engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# sync_engine = create_engine(SYNC_SQLALCHEMY_DATABASE_URL, echo=True)
+sync_engine = create_engine(SYNC_SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=sync_engine)
 
 
 @pytest.fixture
-def session():
+def sync_session():
     Base.metadata.drop_all(bind=sync_engine)
     Base.metadata.create_all(bind=sync_engine)
 
@@ -26,12 +27,12 @@ def session():
 
 
 @pytest.fixture
-def client(session):
+def sync_client(sync_session):
     def override_get_db():
         try:
-            yield session
+            yield sync_session
         finally:
-            session.close()
+            sync_session.close()
 
     # app에서 사용하는 DB를 오버라이드하는 부분
     app.dependency_overrides[get_db] = override_get_db
--- tests/test_sync_example.py
+++ tests/test_sync_example.py
@@ -1,8 +1,8 @@
 from src.domains.sync_example.database.models import SyncExample
 
 
-def test_sync_example_생성(client):
-    res = client.post("/api/sync/no-login/sync/example", json={"name": "test1", "description": "test"})
+def test_sync_example_생성(sync_client):
+    res = sync_client.post("/api/sync/no-login/sync/example", json={"name": "test1", "description": "test"})
     new_sync_example = SyncExample(**res.json())
 
     assert res.status_code == 201
