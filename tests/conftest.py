9379a784551960d9d6dd943c9aa98a0e55b4df4b
keemsy <phzzz@naver.com>
2024-05-03 22:45:48 +0900

    PT-19 Edit: conftest.py 내 가독성을 고려한 코드 구조 변경 - 관련된 동기 코드, 비동기 코드끼리 근처 묶음으로 정리 - 비동기 테스트 간, loop closed 문제를 해결하기 위한, event_loop 재정의 추가 - 참고 자료   - https://stackoverflow.com/questions/61022713/pytest-asyncio-has-a-closed-event-loop-but-only-when-running-all-tests   - https://arc.net/l/quote/obnyijxk

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 import pytest_asyncio
 from httpx import AsyncClient
@@ -52,6 +54,44 @@ AsyncTestingSessionLocal = sessionmaker(
 )
 
 
+"""
+pytest_asyncio 의 기본 event_loop fixture 는 scope 가 Function 이며,
+이 때문에 매 번 테스트 함수가 실행될 때 마다 기존 루프가 닫히고 새로운 루프가 생겨 다른 테스트의 session is closed 문제 발생.
+이를 해결하기 위해, 전체 테스트를 싱행하는 동안 Loop를 하나만 사용하도록 session scope 의 event loop fixture 를 새로 정의한다.
+
+참고 자료
+- https://stackoverflow.com/questions/61022713/pytest-asyncio-has-a-closed-event-loop-but-only-when-running-all-tests
+- https://arc.net/l/quote/obnyijxk
+"""
+
+# 방법 1,
+# @pytest_asyncio.fixture(autouse=True, scope="session")
+# def event_loop():
+#     try:
+#         loop = asyncio.get_running_loop()
+#     except RuntimeError:
+#         loop = asyncio.new_event_loop()
+#     yield loop
+#     loop.close()
+
+# 방법 2.
+# @pytest.fixture
+# def event_loop():
+#     yield asyncio.get_event_loop()
+#
+# def pytest_sessionfinish(session, exitstatus):
+#     asyncio.get_event_loop().close()
+
+# 방법 3.
+@pytest_asyncio.fixture(scope="session")
+def event_loop():
+    """Override pytest-asyncio's event loop fixture to session scope."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    asyncio.set_event_loop(loop)
+    yield loop
+    loop.close()
+
+
 @pytest_asyncio.fixture
 async def async_session():
     async with async_engine.begin() as conn:
@@ -65,7 +105,7 @@ async def async_session():
 
 
 @pytest_asyncio.fixture
-async def async_client(async_session):
+async def async_client(async_session, event_loop):
     async def override_get_db():
         yield async_session
 
