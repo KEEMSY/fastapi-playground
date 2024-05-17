0cc11142b17f5493e9ae578275d0280a65170d2e
keemsy <phzzz@naver.com>
2024-05-17 23:30:27 +0900

    PT-13 Edit: AsyncExampleRepository 수정 - 완성 되지 않은 추상 메서드 추가   - 추상 메서드: 유스 케이스 - 추가 개선할 수 있는 사항   - AsyncExampleRepository를 관심사에 따라 인터페이스로 나눠 구성한다.(인터페이스 분리 원칙 준수)   - 인터페이스 분리 원칙이 불가능하다면, 구성(Composition)을 활용 하는 등 다른 방법을 찾아볼 것

--- src/domains/async_example/business/ports/async_example_repository.py
+++ src/domains/async_example/business/ports/async_example_repository.py
@@ -5,6 +5,21 @@ from src.domains.async_example.business.schemas import AsyncExampleSchema
 
 class AsyncExampleRepository(ABC):
     @abstractmethod
+    async def create_async_example(self, async_example: AsyncExampleSchema) -> AsyncExampleSchema:
+        pass
+
+    @abstractmethod
     async def get_async_example(self, example_id: str) -> AsyncExampleSchema:
         pass
+
     @abstractmethod
+    async def get_async_example_list(self, limit, offset, keyword):
+        pass
+
+    @abstractmethod
+    async def update_async_example(self, example_id: str, async_example: AsyncExampleSchema) -> AsyncExampleSchema:
+        pass
+
+    @abstractmethod
+    async def delete_async_example(self, example_id: str) -> bool:
+        pass
