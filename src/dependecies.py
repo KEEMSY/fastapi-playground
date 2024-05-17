644e3197e04a976c45acb6c140c20068f9dccac6
keemsy <phzzz@naver.com>
2024-05-17 23:16:32 +0900

    PT-13 Add: 의존성 주입을 위한 코드 추가 - AsyncExampleRepository 구현체(AsyncExamplePersistenceAdapter)의존성 주입을 위한 코드 추가

---
+++ src/dependecies.py
@@ -0,0 +1,10 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database import get_async_db
+from src.domains.async_example.database.adapters.async_example_persistance_adapter import AsyncExamplePersistenceAdapter
+
+
+async def get_async_example_repository(
+        async_db: AsyncSession = Depends(get_async_db)) -> AsyncExamplePersistenceAdapter:
+    return AsyncExamplePersistenceAdapter(async_db)
