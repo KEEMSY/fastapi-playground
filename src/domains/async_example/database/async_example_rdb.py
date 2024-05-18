156af910e460a6fb96cb367282d19e9840b81f83
keemsy <phzzz@naver.com>
2024-05-18 17:42:48 +0900

    PT-13 Edit: async_example_rdb.py 추가 - rdb(MySQL)을 사용하여, 데이터베이스와 상호작용하는 메서드 처리 - 관련 테스트 코드 작성
    [ 아이디어 ]
    - RDB를 구분한 만큼 NoSQL을 구현체로 새롭게 작성할 수 있다.
    - PersistenceAdapter에서 RDB 및 NoSQL을 적절히 활용하여, 비즈니스 로직을 구현할 수 있다.

---
+++ src/domains/async_example/database/async_example_rdb.py
@@ -0,0 +1,117 @@
+import logging
+
+from sqlalchemy import or_, select, func
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from src.domains.async_example.business.schemas import AsyncExampleSchema, ASyncExampleSchemaList
+from src.domains.async_example.constants import ErrorCode
+from src.domains.async_example.database.models import AsyncExample
+from src.exceptions import handle_exceptions, ExceptionResponse, DLException
+
+logger = logging.getLogger(__name__)
+
+
+class AsyncExampleCRUD:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    @handle_exceptions
+    async def create_async_example(self, async_example: AsyncExampleSchema) -> AsyncExampleSchema:
+        async_example = AsyncExample(
+            name=async_example.name,
+            description=async_example.description
+        )
+        self.db.add(async_example)
+        await self.db.commit()
+        await self.db.refresh(async_example)
+
+        return AsyncExampleSchema.model_validate(async_example)
+
+    @handle_exceptions
+    async def read_async_example_list(self, limit, offset, keyword):
+        # 조건을 설정한다. 기본적으로 모든 데이터를 대상으로 한다.
+        search_condition = True  # 모든 데이터를 반환하는 기본 조건
+
+        if keyword:
+            # 키워드가 주어진 경우 검색 조건을 추가한다.
+            search_condition = or_(
+                AsyncExample.name.ilike(f'%{keyword}%'),
+                AsyncExample.description.ilike(f'%{keyword}%')
+            )
+
+        # 검색 조건을 기반으로 쿼리를 구성한다.
+        query = select(AsyncExample).where(search_condition).order_by(AsyncExample.create_date.desc())
+
+        # 쿼리 실행
+        results = await self.db.execute(query.offset(offset).limit(limit))
+        async_example_list = results.scalars().all()
+        async_example_schema_list = [AsyncExampleSchema.model_validate(async_example) for async_example in
+                                     async_example_list]
+
+        # 전체 개수를 계산하는 쿼리를 생성한다.
+        total_count_query = select(func.count()).select_from(
+            select(AsyncExample).where(search_condition).subquery()
+        )
+
+        # 전체 개수 조회
+        total_count = await self.db.execute(total_count_query)
+        total = total_count.scalar_one()
+
+        return ASyncExampleSchemaList(total=total, example_list=async_example_schema_list)
+
+    @handle_exceptions
+    async def read_fetch_async_example(self, async_example_id: int) -> AsyncExampleSchema:
+        """
+        연관 관계가 있는 경우, 한번에 모두 가져오는 쿼리를 발생시킨다.
+        """
+        try:
+            stmt = (
+                select(AsyncExample)
+                .options(selectinload(AsyncExample.user))
+                .where(AsyncExample.id == async_example_id)
+            )
+            result = await self.db.execute(stmt)
+            async_example = result.scalars().first()
+
+            if async_example is None:
+                logger.error(f"No AsyncExample found with id {async_example_id}")
+                raise ExceptionResponse(error_code=ErrorCode.NOT_FOUND,
+                                        message=f"No AsyncExample found with id {async_example_id}")
+
+            return AsyncExampleSchema.model_validate(async_example)
+
+        except ExceptionResponse as er:
+            raise
+
+        except Exception as e:
+            logger.error(f"Error while retrieving AsyncExample {async_example_id}: {str(e)}")
+            raise DLException(detail=f"Error while retrieving AsyncExample {async_example_id}")
+
+    @handle_exceptions
+    async def update_async_example_v2(self, async_example_schema: AsyncExampleSchema) -> AsyncExampleSchema:
+        async_example = await self.db.get(AsyncExample, async_example_schema.id)
+        if async_example is None:
+            logger.error(f"No AsyncExample found with id {async_example_schema.id}")
+            raise ExceptionResponse(error_code=ErrorCode.NOT_FOUND,
+                                    message=f"No AsyncExample found with id {async_example_schema.id}")
+
+        async_example.name = async_example_schema.name
+        async_example.description = async_example_schema.description
+
+        await self.db.commit()
+        await self.db.refresh(async_example)
+
+        return AsyncExampleSchema.model_validate(async_example)
+
+    @handle_exceptions
+    async def delete_async_example(self, async_example_id: int):
+        async_example = await self.db.get(AsyncExample, async_example_id)
+        if async_example is None:
+            logger.error(f"No AsyncExample found with id {async_example_id}")
+            raise ExceptionResponse(error_code=ErrorCode.NOT_FOUND,
+                                    message=f"No AsyncExample found with id {async_example_id}")
+
+        await self.db.delete(async_example)
+        await self.db.commit()
+        logger.info(f"Deleted AsyncExample with ID {async_example_id}")
---
+++ tests/src/domains/async_example/database/test_async_example_rdb.py
@@ -0,0 +1,149 @@
+import pytest
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.domains.async_example.business.schemas import AsyncExampleSchema
+from src.domains.async_example.constants import ErrorCode
+from src.domains.async_example.database.async_example_rdb import AsyncExampleCRUD
+from src.exceptions import ExceptionResponse
+from tests.src.domains.async_example.async_example_steps import AsyncExampleSteps
+
+from tests.conftest import async_session as async_db, event_loop, setup_database
+
+
+@pytest.mark.asyncio
+class TestAsyncExampleRDB:
+    @pytest.fixture(autouse=True)
+    def setup(self, async_db: AsyncSession):
+        self.async_example_rdb = AsyncExampleCRUD(async_db)
+
+    async def test_async_example_생성_테스트(self):
+        # given
+        async_example_schema = await AsyncExampleSteps.AsyncExample_스키마_생성(
+            name="test_name", description="test_description"
+        )
+
+        # when
+        async_example_schema = await self.async_example_rdb.create_async_example(
+            async_example_schema)
+        expected = await self.async_example_rdb.read_fetch_async_example(
+            async_example_id=async_example_schema.id
+        )
+
+        # then
+        assert async_example_schema.name == expected.name
+        assert async_example_schema.description == expected.description
+
+    async def test_async_example_조회_테스트(self):
+        # given
+        async_example_schema = await AsyncExampleSteps.AsyncExample_스키마_생성(
+            name="test_name", description="test_description"
+        )
+        async_example_schema = await self.async_example_rdb.create_async_example(
+            async_example_schema)
+
+        # when
+        expected: AsyncExampleSchema = await self.async_example_rdb.read_fetch_async_example(
+            async_example_id=async_example_schema.id
+        )
+
+        # then
+        assert async_example_schema.name == expected.name
+        assert async_example_schema.description == expected.description
+
+    async def test_async_example_조회_실패_테스트(self):
+        # given
+        not_existed_example_id = 99999
+
+        # when, then
+        with pytest.raises(ExceptionResponse) as exc_info:
+            await self.async_example_rdb.read_fetch_async_example(
+                async_example_id=not_existed_example_id
+            )
+        assert exc_info.value.error_code == ErrorCode.NOT_FOUND
+        assert str(exc_info.value.message) == f"No AsyncExample found with id {not_existed_example_id}"
+
+    async def test_async_example_리스트_조회_테스트(self):
+        # given
+        async_example_schema = await AsyncExampleSteps.AsyncExample_스키마_생성(
+            name="test_name", description="test_description"
+        )
+
+        n = 10
+        for _ in range(n):
+            await self.async_example_rdb.create_async_example(async_example_schema)
+
+        # when
+        async_example_list = await self.async_example_rdb.read_async_example_list(
+            limit=10, offset=0, keyword=None
+        )
+
+        # then
+        assert len(async_example_list.example_list) == n
+
+    async def test_async_example_수정_테스트(self):
+        # given
+        async_example_schema = await AsyncExampleSteps.AsyncExample_스키마_생성(
+            name="test_name", description="test_description"
+        )
+        async_example_schema = await self.async_example_rdb.create_async_example(
+            async_example_schema)
+        update_async_example_schema = await AsyncExampleSteps.AsyncExample_스키마_생성(
+            name="updated_name", description="updated_description"
+        )
+        update_async_example_schema.id = async_example_schema.id
+
+        # when
+        updated_async_example = await self.async_example_rdb.update_async_example_v2(
+            async_example_schema=update_async_example_schema
+        )
+
+        # then
+        assert updated_async_example.name == update_async_example_schema.name
+        assert updated_async_example.description == update_async_example_schema.description
+
+    async def test_async_example_수정_실패_테스트(self):
+        # given
+        not_existed_example_id = 99999
+        update_async_example_schema = await AsyncExampleSteps.AsyncExample_스키마_생성(
+            name="updated_name", description="updated_description"
+        )
+        update_async_example_schema.id = not_existed_example_id
+
+        # when, then
+        with pytest.raises(ExceptionResponse) as exc_info:
+            await self.async_example_rdb.update_async_example_v2(
+                async_example_schema=update_async_example_schema
+            )
+        assert exc_info.value.error_code == ErrorCode.NOT_FOUND
+        assert str(exc_info.value.message) == f"No AsyncExample found with id {update_async_example_schema.id}"
+
+    async def test_async_example_삭제_테스트(self):
+        # given
+        async_example_schema = await AsyncExampleSteps.AsyncExample_스키마_생성(
+            name="test_name", description="test_description"
+        )
+        async_example_schema = await self.async_example_rdb.create_async_example(
+            async_example_schema)
+
+        # when
+        await self.async_example_rdb.delete_async_example(
+            async_example_id=async_example_schema.id
+        )
+
+        # then
+        with pytest.raises(ExceptionResponse) as exc_info:
+            await self.async_example_rdb.delete_async_example(
+                async_example_id=async_example_schema.id
+            )
+
+    async def test_async_example_삭제_실패_테스트(self):
+        # given
+        not_existed_example_id = 99999
+
+        # when, then
+        with pytest.raises(ExceptionResponse) as exc_info:
+            await self.async_example_rdb.delete_async_example(
+                async_example_id=not_existed_example_id
+            )
+        assert exc_info.value.error_code == ErrorCode.NOT_FOUND
+        assert str(exc_info.value.message) == f"No AsyncExample found with id {not_existed_example_id}"
